1299265663a48ce90b0e4a531b53979860b63ed1
mhegglinmichael <mhegglinmichael@gmail.com>
2018-09-21 16:53:42 -0700

    finished

--- Lab00.java
+++ Lab00.java
@@ -1,8 +1,40 @@
 public class Lab00
 {
-   public static void main(String[] args)
-   {
+    public static void main(String[] args)
+    {
+        int x = 5;
+        String y = "hello";
+        double z = 9.8;
 
-   
-   }
+        System.out.printf("x: %d y: %s z: %.1f\n", x, y, z);
+
+        int[] nums = {3, 6, -1, 2};
+        for(int i = 0; i < nums.length; i++){
+            System.out.println(nums[i]);
+        }
+
+        int numFound = charCount(y, 'l');
+        System.out.printf("Found: %d\n", numFound);
+
+        for(int i = 1; i < 11; i++){
+            System.out.print(i);
+            System.out.print(" ");
+        }
+
+        System.out.println();
+
+    }
+
+    public static int charCount(String s, char c){
+
+        int count = 0;
+
+        for(char i : s.toCharArray()){
+            if(i == c){
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
